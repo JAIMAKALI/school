50d23de72a5d7f52a9c0a11787e54c0a604b98d0
JAIMAKALI <mk6598951@gmail.com>
2018-08-06 15:19:11 +0530

    changing ui

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home', {layout: false});
 });
+
+router.post('/send_mail',(req,res)=>{
+  console.log(req.body);
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+          user: 'mk6598951@gmail.com',
+          pass: 'Nitp@12345'
+      }
+   });
+
+  const mailOptions = {
+    from: 'mk6598951@gmail.com', // sender address
+    to: 'ravikumarfiitjee@gmail.com', // list of receivers
+    subject: 'Subject of your email', // Subject lin
+    html: '<p>Your html here</p>'// plain text body
+  }; 
+  
+  transporter.sendMail(mailOptions, function (err, info) {
+    if(err)
+      console.log(err)
+    else
+      console.log(info);
+  });
+})
 
 module.exports = router;
