b4e9fd595b1b1c03fce96d191a9bc23cae634e33
JAIMAKALI <mk6598951@gmail.com>
2018-08-07 20:38:51 +0530

    adding email

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var nodemailer = require('nodemailer');
+var hbs = require('nodemailer-express-handlebars');
+var xoauth2    = require('xoauth2');
 
 var router = express.Router();
 
@@ -9,20 +11,47 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/send_mail',(req,res)=>{
-  console.log(req.body);
+  
+  var subject = req.body.Subject;
+  var from    = req.body.Email;
+  var message = req.body.Message;
+
+  var options = {
+    viewEngine: {
+        extname: '.hbs',
+        layoutsDir: 'views/',
+        defaultLayout : 'layout',
+        partialsDir : 'views/partials/'
+    },
+    viewPath: 'views/',
+    extName: '.hbs'
+  };
+
+
   var transporter = nodemailer.createTransport({
     service: 'gmail',
-    auth: {
-          user: 'mk6598951@gmail.com',
-          pass: 'Nitp@12345'
-      }
-   });
+    auth:{   
+          user: 'eduvysupervision@gmail.com',
+          pass:'Eduvy@123',
+          clientId:'490182794397-i958cpolm0vkbr7gf8iovbuoa4akk7s2.apps.googleusercontent.com',
+          clientSecret: 'WJQnKpJI0JiF9-NymqJI6msZ',
+          refreshToken:'1/CsM4ORXQbm8Z4Fmpums905CCWwMV-OKC0vNH5pPAHL5nvC6J1i-QndxU26BR5bMn',
+          accessToken:'ya29.GlvxBSEcNNmVLRTFFkZHnihd5W7vB7t-doeZTfbBD--9xTzmoHZi369BjrWKHV89Y6BWWD7R_X94_wsI7RfQX8UnXhLON34afwQs9PXCQVVH-GD2P5F40VOTWHjG'
+    
+    }
+  });
+
+  transporter.use('compile', hbs(options));
 
   const mailOptions = {
-    from: 'mk6598951@gmail.com', // sender address
-    to: 'ravikumarfiitjee@gmail.com', // list of receivers
-    subject: 'Subject of your email', // Subject lin
-    html: '<p>Your html here</p>'// plain text body
+    from: from, // sender address
+    to: 'mk6598951@gmail.com', // list of receivers  id 15082829077-c27hlmg5rt0o821nk9l7no8763j72d0f.apps.googleusercontent.com
+    subject: subject, // Subject lin       seceret  9tuDKQHbnPZrzMkwXsaOu_zl
+    template: 'email_body',
+     context: {
+          variable1 : 'value1',
+          variable2 : 'value2'
+    }
   }; 
   
   transporter.sendMail(mailOptions, function (err, info) {
@@ -30,7 +59,9 @@ router.post('/send_mail',(req,res)=>{
       console.log(err)
     else
       console.log(info);
+      res.render('home', {layout: false});
   });
+ 
 })
 
 module.exports = router;
